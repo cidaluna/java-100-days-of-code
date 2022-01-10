64f3bf866b1590c6800e9fffd7f05c3ef62f9b50
cidaluna <cidalunna@hotmail.com>
2022-01-10 09:50:42 -0300

    Day 7 prime number between 1 and 500

---
+++ src/main/java/exercisesJava/Day7.java
@@ -0,0 +1,33 @@
+package exercisesJava;
+
+public class Day7 {
+    public static void main(String[] args) {
+        // numeros primos só podem ser aqueles dividos por 1 e por ele mesmo
+        // 0 e 1 não são primos
+        // 2 é o único número par primo
+        // verificar de 1 a 500 quais são os números primos
+
+        int inicio = 1;
+        int fim = 500;
+        System.out.println("Os numeros primos são: ");
+        for (int i = inicio; i <= fim; i++) {
+            if (verificaNumerosPrimos(i) == 1) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+
+    public static int verificaNumerosPrimos(int num) {
+        int contador = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                contador = contador + 1;
+            }
+        }
+        if (contador == 2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
