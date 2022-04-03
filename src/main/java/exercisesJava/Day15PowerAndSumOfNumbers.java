66f69d5ced7df0cdb2fb6f842e20191b64a05118
cidaluna <cidalunna@hotmail.com>
2022-04-03 11:57:39 -0300

    update variable contador

---
+++ src/main/java/exercisesJava/Day15PowerAndSumOfNumbers.java
@@ -0,0 +1,16 @@
+package exercisesJava;
+
+import java.math.BigInteger;
+
+public class Day15PowerAndSumOfNumbers {
+    /*
+    * Day 15 - Project Euler 16 - Power and the sum of numbers
+    * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+    */
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
